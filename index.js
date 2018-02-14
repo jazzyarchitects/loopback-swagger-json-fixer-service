90fb62fe3ebee117ea22f5dcdd9407726e4c0713
Jibin Mathews <jazzyarchitects@users.noreply.github.com>
2018-02-14 16:37:59 +0530

    Removing need for SERVER_HOST

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const rootpath = process.cwd();
 
 const fileName = process.argv[2];
 
-if (!process.env.SERVER_HOST) {
-  console.log(
-    colors.red.bold('Error: '),
-    colors.red(
-      "Pass the this server's host as SERVER_HOST environment variable along with protocol to use. Eg: http://localhost:3002",
-    ),
-  );
-  process.exit(1);
-}
+// if (!process.env.SERVER_HOST) {
+//   console.log(
+//     colors.red.bold('Error: '),
+//     colors.red(
+//       "Pass the this server's host as SERVER_HOST environment variable along with protocol to use. Eg: http://localhost:3002",
+//     ),
+//   );
+//   process.exit(1);
+// }
 
 if (!fileName) {
   console.log(colors.red.bold('Error: '), colors.red('Pass the file name as the first argument'));
@@ -38,17 +38,17 @@ if (path.extname(filePath) !== '.json') {
   process.exit(1);
 }
 
-const splits = process.env.SERVER_HOST.split('://');
-const scheme = splits[0];
-const host = splits[1];
+// const splits = process.env.SERVER_HOST.split('://');
+// const scheme = splits[0];
+// const host = splits[1];
 
-if (!['http', 'https'].includes(scheme)) {
-  console.log(
-    colors.red.bold('Erorr:'),
-    colors.red("API protocol should be one of ['http', 'https']"),
-  );
-  process.exit(1);
-}
+// if (!['http', 'https'].includes(scheme)) {
+//   console.log(
+//     colors.red.bold('Erorr:'),
+//     colors.red("API protocol should be one of ['http', 'https']"),
+//   );
+//   process.exit(1);
+// }
 
 const originalContent = require(filePath);
 
@@ -56,7 +56,8 @@ const { paths } = originalContent;
 
 const fixedObject = JSON.parse(JSON.stringify(originalContent));
 
-(fixedObject.schemes = [scheme]), (fixedObject.host = host);
+fixedObject.schemes = ["https", "http"];
+// fixedObject.host = host;
 
 Object.keys(paths).forEach(_path => {
   const methods = Object.keys(paths[_path]);
